ade7902966a73f9d4b02bb92e067bd93d9625e72
Lakshmi Mulinti <lakshmimulinti70@gmail.com>
2026-07-11 15:25:28 +0530

    Replace Resend with Nodemailerss

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // true only for 465
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
